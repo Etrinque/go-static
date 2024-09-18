470d823d6e3a9e594ca1c0d392fd094aa195bc7a
Eric Trinque <solvdinc@outlook.com>
2024-09-18 13:14:54 -0400

    go-static main and template added

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
+const DIST string = "./dist"
+const CONTENT string = "./content"
+const TEMPLATE string = "./template.html"
+const STATIC string = "./static"
+const SITE_GEN_ERROR string = "error gen item"
+
 func main() {
-	fmt.Println("Static Site gen")
-	var siteGenError = errors.New("error gen item")
-	fmt.Print(siteGenError)
+	// var err = errors.New(SITE_GEN_ERROR)
+	// fmt.Println("Static Site gen")
+	// fmt.Println(err)
+	// fmt.Println(DIST)
+
+	fmt.Println("Copying Files To Distribution Directory")
+	copyFiles(STATIC, DIST)
+
+	fmt.Println("Generating Sites From Content Directory")
+	genContent(CONTENT, TEMPLATE, DIST)
+
 }
