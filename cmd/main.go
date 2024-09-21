12780837bc4947e0490bf69da5618cec14092a28
Eric Trinque <solvdinc@outlook.com>
2024-09-21 10:39:39 -0400

    -

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"runtime"
 )
 
 const DIST string = "./dist"
@@ -12,10 +14,8 @@ const STATIC string = "./static"
 const SITE_GEN_ERROR string = "error gen item"
 
 func main() {
-	// var err = errors.New(SITE_GEN_ERROR)
-	// fmt.Println("Static Site gen")
-	// fmt.Println(err)
-	// fmt.Println(DIST)
+	fmt.Println("Static Site gen")
+	fmt.Println(DIST)
 
 	// fmt.Println("Copying Files To Distribution Directory")
 	// copyFiles(STATIC, DIST)
@@ -25,5 +25,21 @@ func main() {
 
 	component := hello("Eric")
 	component.Render(context.Background(), os.Stdout)
+	PrintMemUsage()
 
+}
+
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Print("\n==================\n")
+	fmt.Printf("Alloc = %v MiB\n", bToMb(m.Alloc))
+	fmt.Printf("TotalAlloc = %v MiB\n", bToMb(m.TotalAlloc))
+	fmt.Printf("Sys = %v MiB\n", bToMb(m.Sys))
+	fmt.Printf("NumGC = %v\n", m.NumGC)
+	fmt.Print("==================\n")
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
