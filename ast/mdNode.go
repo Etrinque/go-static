b34c94e21bdb7216e23d19380b0224c4faebd969
Etrinque <Solvdinc@outlook.com>
2024-09-26 21:45:22 -0400

    added static map factory function

--- ast/mdNode.go
+++ ast/mdNode.go
@@ -3,7 +3,9 @@ package ast
 import "time"
 
 type MDNode struct {
-	Type *NType
+	BlockType *NType
+	BLockTag map[string]string
+	Content *Content
 	Meta *Meta
 }
 
@@ -15,32 +17,76 @@ type Meta struct {
 	createdAt time.Time
 }
 
-const (
-	NONE            = iota
-	H1              //	#
-	H2              //	##
-	H3              //	###
-	H4              //	####
-	H5              //	#####
-	H6              //	######
-	ITALIC          //	* text *
-	BOLD            //	** text **
-	BLOCKQUOTE      //	>
-	ORDERED_LIST    //	1. 2. 3. etc.
-	UNORDERED_LIST  //	- Item - Item - Item etc.
-	CODE            //	` code here `
-	HORIZONTAL_RULE //	---
-	LINK            //	[title](link)
-	IMAGE           // ![alt text](img link)
-	TABLE           // |	Thing1	 |	Thing2	|
-	FENCED_CODE     //	```	   Code here	```
-	FOOTNOTE        // [^1]
-	HEADING_ID      // ### HEADING {#custom-id}
-	DEFINITION_LIST //term : definition
-	STRIKETHROUGH   // ~~~ text ~~~
-	TASK_LIST       //-[x] -[] -[] etc.
-	EMOJI           // Who Cares
-	HIGHLIGHT       // == IMPORTANT TEXT ==
-	SUBSCRIPT       //H~2~0
-	SUPERSCRIPT     //X^2^
-)
+// BlockTag is the representation of MD tags, mapped to the TagLabel
+type BlockTag map[string]string
+
+// Content is a section of Markdown after being parsed that retains the BlockTag Open, Close, and the Content within tags
+type Content struct {
+	TagLabel *BlockTag
+	Open string
+	Close string
+	InnerContent string
+}
+
+func genMap() map[string]string {
+	var MdTypeMap = map[string]string{
+		"":"NONE",            		//  " "
+		"#":"H1",              		//	#
+		"##":"H2",              	//	##
+		"###":"H3",              	//	###
+		"####":"H4",              	//	####
+		"#####":"H5",              	//	#####
+		"######":"H6",              //	######
+		"*":"ITALIC",          		//	* text *
+		"**":"BOLD",            	//	** text **
+		">":"BLOCKQUOTE",      		//	>
+		"1.":"ORDERED_LIST",    	//	1. 2. 3. etc.
+		"- ":"UNORDERED_LIST",  	//	- Item - Item - Item etc.
+		"`":"CODE",            		//	` code here `
+		"---":"HORIZONTAL_RULE", 	//	---
+		"[":"LINK",            	//	[title](link)
+		"![":"IMAGE",           	// ![alt text](img link)
+		"| ":"TABLE",           	// |	Thing1	 |	Thing2	|
+		"```":"FENCED_CODE",     	//	```	   Code here	```
+		"[^":"FOOTNOTE",        	// [^1]
+		// "":"HEADING_ID",      	// ### HEADING {#custom-id}
+		" : ":"DEFINITION_LIST", 	//term : definition
+		"~~~ ":"STRIKETHROUGH",   	// ~~~ text ~~~
+		"-[":"TASK_LIST",       	//-[x] -[] -[] etc.
+		// "":"EMOJI",           	// Who Cares
+		"== ":"HIGHLIGHT",       	// == IMPORTANT TEXT ==
+		// "":"SUBSCRIPT",       	//H~2~0
+		// "":"SUPERSCRIPT"     	//X^2^
+	}
+	return MdTypeMap
+}
+
+// const (
+	// NONE            = iota
+	// H1              //	#
+	// H2              //	##
+	// H3              //	###
+	// H4              //	####
+	// H5              //	#####
+	// H6              //	######
+	// ITALIC          //	* text *
+	// BOLD            //	** text **
+	// BLOCKQUOTE      //	>
+	// ORDERED_LIST    //	1. 2. 3. etc.
+	// UNORDERED_LIST  //	- Item - Item - Item etc.
+	// CODE            //	` code here `
+	// HORIZONTAL_RULE //	---
+	// LINK            //	[title](link)
+	// IMAGE           // ![alt text](img link)
+	// TABLE           // |	Thing1	 |	Thing2	|
+	// FENCED_CODE     //	```	   Code here	```
+	// FOOTNOTE        // [^1]
+	// HEADING_ID      // ### HEADING {#custom-id}
+	// DEFINITION_LIST //term : definition
+	// STRIKETHROUGH   // ~~~ text ~~~
+	// TASK_LIST       //-[x] -[] -[] etc.
+	// EMOJI           // Who Cares
+	// HIGHLIGHT       // == IMPORTANT TEXT ==
+	// SUBSCRIPT       //H~2~0
+	// SUPERSCRIPT     //X^2^
+// )
