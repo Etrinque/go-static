e9cf7c91c0783852792a31b2de4233a7de4d9ec7
Eric Trinque <solvdinc@outlook.com>
2024-09-30 11:50:38 -0400

    started flesh out htmlnode structure

--- ast/htmlNode.go
+++ ast/htmlNode.go
@@ -1,1 +1,21 @@
 package ast
+
+type HTMLNode struct {
+	TagType  *HTMLType
+	BlockTag *HTMLBlockTag
+	Content  *Content
+}
+
+type HTMLType struct {
+	Name string
+	Tag  string
+}
+
+type Headers struct {
+	Tag  string
+	Meta *MDMeta
+}
+
+type HTMLBlockTag map[string]string
+
+func (HTMLNode) GenMap()
