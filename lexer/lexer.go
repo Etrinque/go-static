c0b44cd98766d111292b0605e0eca7faa27b005e
Eric Trinque <solvdinc@outlook.com>
2024-10-01 11:17:37 -0400

    structure, init and read added

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,3 +1,29 @@
 package lexer
 
-// package lexer is responsiple for the lexical analysis of MD files
+/*
+package Lexer is responsiple for the lexical analysis of MD files.  It implements the ast/mdnode elements. Lexer is implemented by the Parser pkg. It crawls through the document char by char and tracks it position and relative chars
+*/
+
+type Lexer struct {
+	input        []byte
+	position     int
+	readPosition int
+	char         byte
+}
+
+func New(input []byte) *Lexer {
+	lex := &Lexer{
+		input: input,
+	}
+	return lex
+}
+
+func (lex *Lexer) readChar() {
+	if lex.readPosition >= len(lex.input) {
+		lex.char = 0
+	} else {
+		lex.char = lex.input[lex.readPosition]
+	}
+	lex.position = lex.readPosition
+	lex.readPosition += 1
+}
