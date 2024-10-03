039f8660bd17ddb1ae0ac718773e30eeb208d770
Eric Trinque <solvdinc@outlook.com>
2024-10-03 18:49:21 -0400

    parser file read func added

--- parser/parser.go
+++ parser/parser.go
@@ -1,4 +1,34 @@
 package parser
 
+import (
+	"go-static/ast"
+	"go-static/lexer"
+	"os"
+)
+
 // package parser is responsible for Creating and implementing the parsing program.
 // It defines the
+
+type Parser struct {
+	Lex      *lexer.Lexer
+	currChar byte
+	peekChar byte
+}
+
+type Parsed struct {
+	Nodes []ast.MDNode
+}
+
+func NewParser(input []byte) *Parser {
+	return &Parser{
+		Lex: lexer.New(input),
+	}
+}
+
+func ReadFile(path string) ([]byte, error) {
+	buf, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return buf, nil
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,1 +1,12 @@
 package parser
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestReadFile(t *testing.T) {
+	file, _ := ReadFile("C:/Users/SOLVD/github_repos/go-static/content/markdown-cheat-sheet.md")
+	// p := parser.NewParser(file)
+	fmt.Println(string(file))
+}
