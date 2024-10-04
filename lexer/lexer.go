b9d6f71a1e43163efb1edd702b8a3716e47ca1ef
Etrinque <Solvdinc@outlook.com>
2024-10-04 17:57:06 -0400

    -

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,9 @@
 package lexer
 
+import (
+	"go-static/ast"
+)
+
 /*
 package Lexer is responsiple for the lexical analysis of MD files.  It implements the ast/mdnode elements. Lexer is implemented by the Parser pkg. It crawls through the document char by char and tracks it position and relative chars
 */
@@ -27,4 +31,30 @@ func (lex *Lexer) readChar() {
 	}
 	lex.position = lex.readPosition
 	lex.readPosition += 1
+}
+
+func(lex *Lexer) nextChar() {}
+
+
+// buildStr walks through input and builds tags as they come based on the tagMap
+func(lex *Lexer) buildStr() string {
+	return ""
+}
+
+func(lex *Lexer) checkTagMap(tagmap ast.MDTagMap) bool {
+	var chars = "!["
+	for k, _ := range(tagmap) {
+		if chars != k {
+			return false
+		} else {
+			switch chars {
+			case "![":
+				return true
+			default:
+				return true
+			}
+		}
+	}
+
+	return false
 }
