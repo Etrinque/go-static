753b11f14aef3bdfd5339f7e450768f0af5359a8
Eric Trinque <solvdinc@outlook.com>
2024-10-07 17:15:02 -0400

    helper functions created

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "strings"
+
 /*
 package Lexer is responsiple for the lexical analysis of MD files.  It implements the ast/mdnode elements. Lexer is implemented by the Parser pkg. It crawls through the document char by char and tracks it position and relative chars
 */
@@ -27,4 +29,48 @@ func (lex *Lexer) readChar() {
 	}
 	lex.position = lex.readPosition
 	lex.readPosition += 1
+}
+
+func (lex *Lexer) nextChar(char byte) {}
+
+func (lex *Lexer) peekChar() {}
+
+func (lex *Lexer) lexTag() {}
+
+func (lex *Lexer) buildTag(char byte) []byte {
+	var tag []byte
+
+	tag = append(tag, char)
+
+	return tag
+}
+
+func (lex *Lexer) buildBlock(char byte) ([]byte, error) {
+	var builder strings.Builder
+	var block []byte
+
+	block = append(block, char)
+
+	_, err := builder.Write(block)
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(builder.String()), nil
+}
+
+func (lex *Lexer) isAlpha(char byte) bool {
+	if char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z' {
+		return true
+	}
+
+	return false
+}
+
+func (lex *Lexer) isDigit(char byte) bool {
+	if char >= '0' && char <= '9' {
+		return true
+	}
+
+	return false
 }
